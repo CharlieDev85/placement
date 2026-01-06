d6dec074c25702ba253032b7adbfdf7e7dcc6dd8
Carlos Enrique Marroquín <charliemarroq@gmail.com>
2026-01-06 16:49:28 -0600

    Add enrollment flow for recommended courses

--- src/main/java/com/blue/app/service/RecommendationMadeService.java
+++ src/main/java/com/blue/app/service/RecommendationMadeService.java
@@ -1,8 +1,11 @@
 package com.blue.app.service;
 
 import com.blue.app.model.RecommendationMade;
+import com.blue.app.model.Student;
 import com.blue.app.repository.RecommendationMadeRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class RecommendationMadeService {
@@ -14,5 +17,9 @@ public class RecommendationMadeService {
 
     public RecommendationMade save(RecommendationMade recommendationMade) {
         return recommendationMadeRepository.save(recommendationMade);
+    }
+
+    public Optional<RecommendationMade> findLatestForStudent(Student student) {
+        return recommendationMadeRepository.findTopByAttemptStudentOrderByCreatedAtDesc(student);
     }
 }
