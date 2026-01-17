940b8627599dc54c13373cd40fccd2537f4c7703
Carlos Marroquin <carlosmarroquin@Carloss-MacBook-Air.local>
2026-01-16 18:40:03 -0600

    still moving logo

--- src/main/java/com/blue/ui/LandingView.java
+++ src/main/java/com/blue/ui/LandingView.java
@@ -15,7 +15,7 @@ public class LandingView extends VerticalLayout {
         setJustifyContentMode(JustifyContentMode.CENTER);
         setSizeFull();
 
-        Image logo = new Image("img/blue-academy-logo-landing.svg", "Logo");
+        Image logo = new Image("logos/blue-academy-logo-landing.svg", "Logo");
         logo.setHeight("100px");
 
         H1 title = new H1("Test Your English Skills!");
